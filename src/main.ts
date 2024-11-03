a40da4e0442ca7022e5369b6834975ca7efe45de
SunnatilloDev <ytraderoearning@gmail.com>
2024-11-03 16:08:11 +0500

    upload done

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,26 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { config } from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express'; // Use named import
+
 config();
-import express from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
+
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('CryptoArticle API')
     .setDescription('API documentation')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api/docs', app, document);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  app.use("/uploads", express.static("uploads"))
+  app.use('/uploads', express.static('uploads')); // Use named import
+
   await app.listen(8080);
 }
+
 bootstrap();
