c48da35f50a8dd13d0b80383f4916234d6055464
SunnatilloDev <ytraderoearning@gmail.com>
2024-11-18 21:43:44 +0500

    done

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
+import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
-import { diskStorage } from 'multer';
-import * as path from 'path';
-import { v4 } from 'uuid';
 
 @Module({
-  imports: [
-    MulterModule.register({
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, cb) => {
-          const fileName = v4() + path.extname(file.originalname); // Generate unique name
-          cb(null, fileName);
-        },
-      }),
-    }),
-  ],
+  controllers: [UploadController],
   providers: [UploadService],
-  exports: [UploadService],
 })
-export class AppModule {}
+export class UploadModule {}
