53e601ea920d4d3380e43b9ff50895bb4f37e304
SunnatilloDev <ytraderoearning@gmail.com>
2024-11-30 20:40:32 +0500

    some

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,16 @@ import { ValidationPipe, Logger } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import * as compression from 'compression';
 import helmet from 'helmet';
-
+const express = require('express');
 async function bootstrap() {
   // Load env variables early
   const logger = new Logger('Bootstrap');
   logger.log('Loading environment variables...');
-  
+
   // Log the current working directory and NODE_ENV
   logger.debug(`Current working directory: ${process.cwd()}`);
   logger.debug(`NODE_ENV: ${process.env.NODE_ENV}`);
-  
+
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
   });
@@ -46,19 +46,26 @@ async function bootstrap() {
   // Swagger documentation
   const config = new DocumentBuilder()
     .setTitle('Crypto Article API')
-    .setDescription('API for managing crypto-related articles and user subscriptions')
+    .setDescription(
+      'API for managing crypto-related articles and user subscriptions',
+    )
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-  
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // Static files
+  app.use('uploads', express.static('uploads'));
 
   // Start server
   const port = process.env.PORT || 8080;
   await app.listen(port);
   logger.log(`Application is running on: http://localhost:${port}`);
-  logger.log(`Swagger documentation available at: http://localhost:${port}/api`);
+  logger.log(
+    `Swagger documentation available at: http://localhost:${port}/api`,
+  );
 }
 
 bootstrap();
