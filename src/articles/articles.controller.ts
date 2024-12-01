594c8edc01096a869f75032a1bb24b3faffa1870
SunnatilloDev <ytraderoearning@gmail.com>
2024-12-01 20:09:58 +0500

    update

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -50,8 +50,8 @@ export class ArticlesController {
   @ApiOperation({ summary: 'Retrieve all articles' })
   @ApiResponse({ status: 200, description: 'List of all articles.' })
   @ApiResponse({ status: 500, description: 'Failed to retrieve articles.' })
-  async getAllArticles() {
-    return await this.articleService.getAllArticles();
+  async getAllArticles(@Body() userId?: number) {
+    return await this.articleService.getAllArticles(userId);
   }
 
   @Get('search')
